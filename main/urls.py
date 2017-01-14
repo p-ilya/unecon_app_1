62a30c4b623a72885615136e8204831bf2852c42
Elias <pyatinson@gmail.com>
2017-01-14 05:28:00 -0600

    Pages added

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^rasp/$', views.rasp, name='criteria'),
+    url(r'^info/$', views.info, name='info'),
 ]
    
--- main/views.py
+++ main/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'main/index.html', {})
+
+def rasp(request):
+    return render(request, 'main/rasp.html', {})
+
+def info(request):
+    return render(request, 'main/info.html', {})
